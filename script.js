2921b1ff88fbefff17928c420964ef4bf076e950
HelloSeva <sevda.seyidova611@gmail.com>
2023-01-10 00:41:20 +0000

    Validate password length ensured

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ function generatePassword() {
 
 // variables for prompts //
 var passwordLength = prompt("Please enter your length of the password,from 10 characters to 64 characters.");
+// Validate password length
+while (passwordLength < 10 || passwordLength > 64) {
+  passwordLength = prompt("Invalid length! Enter a number between 10 and 64:");
+}
+
 var upperCase = confirm("Click OK to confirm including uppercase charecters");
 var lowerCase = confirm("Click OK to confirm including lowercase charecters");
 var numbers  = confirm("Click OK to confirm including numeric characters");
 var specialCh = confirm("Click OK to confirm including special characters");
+// Validate character types
+while (!lowerCase && !upperCase && !numbers && !specialCh) {
+  alert("You must select at least one character type!");
+  lowerCase = confirm("Include lowercase characters in the password?");
+  upperCase = confirm("Include uppercase characters in the password?");
+  numbers = confirm("Include numeric characters in the password?");
+  specialCh = confirm("Include special characters in the password?");
+}
 
 // Array of possible characters to be included in password //
 var password = "";
@@ -116,16 +129,20 @@ if (specialCh === true) {
   allCharacters.push.apply(allCharacters, specialCharacters);
 }
 
+
 // makes password random and the exact length selected by the user //
 for (var i=0; i<passwordLength; i++) {
   var random = Math.floor(Math.random() * allCharacters.length);
   password = password + allCharacters[random];
 }
+
+
 // console.log(generated Password)
 return password;
 }
 
 
+//alredy working code 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
@@ -139,3 +156,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
